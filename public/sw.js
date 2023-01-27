7a4e6ec2ecc87a82fe16195190af0abdc12dc3f7
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2023-01-27 15:07:41 +0000

    Fixed fetch event and removed express import

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,3 @@
-const { request } = require("express");
-
 const PRECACHE = "precache-v1";
 const RUNTIME = "runtime";
 
@@ -51,20 +49,6 @@ self.addEventListener("activate", (event) => {
 
 self.addEventListener("fetch", (event) => {
 	if (event.request.url.startsWith(self.location.origin)) {
-		event.respondWith(
-			caches.match(event.request).then((cachedResponse) => {
-				if (event.request.method === "GET" && cachedResponse) {
-					return cachedResponse;
-				}
-
-				return caches.open(RUNTIME).then((cache) => {
-					return fetch(event.request).then((response) => {
-						return cache.put(event.request, response.clone()).then(() => {
-							return response;
-						});
-					});
-				});
-			})
-		);
+		event.respondWith(fetch(event.request));
 	}
 });
