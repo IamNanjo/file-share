379af985ca27ce0ba98ceabfe70abb064f99f44d
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2024-01-28 14:58:56 +0200

    Prepare file upload

---
+++ server/api/upload/index.post.ts
@@ -0,0 +1,24 @@
+import { randomUUID } from "node:crypto";
+import z from "zod";
+
+import getServerSession from "~/server/getServerSession";
+import db from "~/server/db";
+
+const bodySchema = z.object({ name: z.string(), extension: z.string() });
+
+export default defineEventHandler(async (e) => {
+  const body = await readValidatedBody(e, (body) =>
+    bodySchema.parseAsync(body)
+  );
+  const session = await getServerSession(e);
+
+  if (!session || !session.user || !session.user.id)
+    return setResponseStatus(e, 401);
+
+  const fullFileName = randomUUID() + body.extension;
+
+  return db.file.create({
+    data: { id: fullFileName, ownerId: session.user.id, name: body.name },
+    select: { id: true },
+  });
+});
