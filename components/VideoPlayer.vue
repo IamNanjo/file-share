da38e1e4ab26a6e29cea6682141fbc87e5f79d46
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2024-01-30 21:56:34 +0200

    Basic video player

---
+++ components/VideoPlayer.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+defineProps<{ url: string }>();
+</script>
+
+<template>
+  <div class="video-player">
+    <video class="video-player__video" controls :src="url" />
+    <div class="video-player__controls"></div>
+  </div>
+</template>
+
+<style>
+.video-player {
+  display: flex;
+  flex-direction: column;
+  max-width: 98%;
+  aspect-ratio: 16 / 9;
+
+  > * {
+    width: 100%;
+  }
+}
+</style>
---
+++ pages/watch/[id].vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <VideoPlayer :url="`/files/${$route.params.id}`" />
+  </main>
+</template>
+
+<style scoped lang="scss">
+main {
+  justify-content: flex-start;
+  align-items: center;
+  padding-block: 2em;
+}
+</style>
