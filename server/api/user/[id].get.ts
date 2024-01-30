b2527d64a4afa43eeb01aa6df7a7b0df325ff4a8
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2024-01-30 22:02:23 +0200

    Get user's files

---
+++ server/api/user/[id].get.ts
@@ -0,0 +1,25 @@
+import db from "~/server/db";
+
+export default defineEventHandler((e) => {
+  const id = getRouterParam(e, "id");
+
+  if (!id) return setResponseStatus(e, 400);
+
+  return db.user.findUnique({
+    where: { id },
+    select: {
+      id: true,
+      name: true,
+      files: {
+        select: {
+          id: true,
+          name: true,
+          type: true,
+          sizeString: true,
+          created: true,
+        },
+        orderBy: { created: "desc" },
+      },
+    },
+  });
+});
