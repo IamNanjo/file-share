f2c1c287520b88d64f2f7c71ec86357ff28235e3
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2024-01-30 22:02:08 +0200

    File embeds implemented

---
+++ server/routes/embed/[id].get.ts
@@ -0,0 +1,45 @@
+import db from "~/server/db";
+
+export default defineEventHandler(async (e) => {
+  const file = await db.file.findUnique({
+    where: { id: e.context.params!.id },
+  });
+
+  if (!file || !file.type) return setResponseStatus(e, 404);
+
+  const url = getRequestURL(e);
+
+  const fileUrl = `${url.origin}/files/${file.id}`;
+
+  if (file.type.includes("video"))
+    return `<!DOCTYPE html>
+<html>
+<head>
+	<meta property="og:type" content="video.other">
+	<meta property="og:title" content="${file?.name}" />
+	<meta property="twitter:player" content="/files/${file.id}">
+	<meta name="twitter:url" content="${fileUrl}" />
+	<meta name="twitter:title" content="${file.name}" />
+	<meta property="og:video:type" content="text/html">
+	<meta property="og:video:width" content="${file.width}">
+	<meta property="og:video:height" content="${file.height}">
+	<meta name="twitter:image" content="${url.origin}/thumbnails/${file.id}">
+	<meta http-equiv="refresh" content="0;url=${fileUrl}">
+</head>
+</html>`;
+
+  if (file.type.includes("image"))
+    return `<!DOCTYPE html>
+<html>
+<head>
+	<meta property="og:title" content="${file?.name}" />
+	<meta property="og:image" content="${fileUrl}" />
+	<meta name="twitter:url" content="${fileUrl}" />
+	<meta name="twitter:title" content="${file.name}" />
+	<meta name="twitter:image" content="${fileUrl}">
+	<meta http-equiv="refresh" content="0;url=${fileUrl}">
+</head>
+</html>`;
+
+  return sendRedirect(e, `${fileUrl}`);
+});
