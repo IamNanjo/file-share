845564fbed1d7426b328dd43d28b1ba3ba580207
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2024-02-01 00:56:13 +0200

    Embed twitter player card

--- server/routes/embed/[id].get.ts
+++ server/routes/embed/[id].get.ts
@@ -3,6 +3,7 @@ import db from "~/server/db";
 export default defineEventHandler(async (e) => {
   const file = await db.file.findUnique({
     where: { id: e.context.params!.id },
+    include: { owner: true },
   });
 
   if (!file || !file.type) return setResponseStatus(e, 404);
@@ -14,17 +15,21 @@ export default defineEventHandler(async (e) => {
   if (file.type.includes("video"))
     return `<!DOCTYPE html>
 <html><head>
-  <meta property="twitter:player" content="${fileUrl}" />
-  <meta name="twitter:card" content="summary" />
-  <meta name="twitter:url" content="${fileUrl}" />
-  <meta name="twitter:title" content="${file.name}" />
-  <meta name="twitter:image" content="${url.origin}/thumbnails/${file.id}" />
   <meta property="og:type" content="video.movie" />
-  <meta property="og:title" content="${file?.name}" />
+  <meta property="og:title" content="${file.name}" />
   <meta property="og:video" content="${fileUrl}" />
   <meta property="og:video:type" content="${file.type}" />
   <meta property="og:video:width" content="${file.width}" />
   <meta property="og:video:height" content="${file.height}" />
+  <meta name="twitter:card" content="player" />
+  <meta name="twitter:site" content="FileShare" />
+  <meta name="twitter:player" content="${fileUrl}" />
+  <meta name="twitter:player:width" content="${file.width}" />
+  <meta name="twitter:player:height" content="${file.height}" />
+  <meta name="twitter:title" content="${file.name}" />
+  <meta name="twitter:image" content="${url.origin}/thumbnails/${file.id}" />
+  <meta name="twitter:creator" content="${file.owner.name}" />
+  <meta name="twitter:creator:id" content="${file.ownerId}" />
   <meta http-equiv="refresh" content="0;url=${url.origin}/watch/${file.id}" />
 </head></html>
 `;
@@ -32,7 +37,7 @@ export default defineEventHandler(async (e) => {
   if (file.type.includes("image"))
     return `<!DOCTYPE html>
 <html><head>
-	<meta property="og:title" content="${file?.name}" />
+	<meta property="og:title" content="${file.name}" />
 	<meta property="og:image" content="${fileUrl}" />
 	<meta name="twitter:url" content="${fileUrl}" />
 	<meta name="twitter:title" content="${file.name}" />
