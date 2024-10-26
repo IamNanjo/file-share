f43985db029dbbab68b331c3c4ecc32ff5000e98
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2024-10-26 08:43:05 +0300

    Packages updated

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,22 +1,26 @@
 export default defineNuxtConfig({
   devtools: { enabled: false },
   vite: { optimizeDeps: { include: ["cookie"] } },
+
   app: {
     pageTransition: { name: "page", mode: "out-in" },
   },
+
   modules: [
     "@nuxtjs/color-mode",
     "@nuxtjs/google-fonts",
     "@nuxtjs/plausible",
     "@hebilicious/authjs-nuxt",
-    "nuxt-icon",
+    "@nuxt/icon",
   ],
+
   colorMode: {
     classPrefix: "",
     classSuffix: "",
     fallback: "dark",
     storageKey: "theme",
   },
+
   googleFonts: {
     families: {
       "Roboto": [400, 500, 700],
@@ -24,18 +28,23 @@ export default defineNuxtConfig({
     },
     display: "swap",
   },
+
   plausible: {
     apiHost: "https://plausible.nanjo.tech",
   },
+
   authJs: {
     baseUrl: process.env.FILESHARE_BASE_URL || "",
     verifyClientOnEveryRequest: true,
     authenticatedRedirectTo: "/",
     guestRedirectTo: "/",
   },
+
   runtimeConfig: {
     authJs: {
       secret: process.env.FILESHARE_SECRET,
     },
   },
+
+  compatibilityDate: "2024-10-18",
 });
