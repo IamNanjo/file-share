0bfdc39d88c9a454b4890a9306a89e175a614bbf
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2024-10-26 19:34:28 +0300

    Formatted code

--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -9,92 +9,98 @@ const runtimeConfig = useRuntimeConfig();
 
 // Get the user using username (case-insensitive)
 const getUser = async (username: string) => {
-  const users: {
-    id: string;
-    name: string;
-    password: string;
-  }[] = await db.$queryRaw`
+    const users: {
+        id: string;
+        name: string;
+        password: string;
+    }[] = await db.$queryRaw`
     SELECT id, name, password
     FROM User
     WHERE name LIKE ${username}
     LIMIT 1`;
 
-  if (!users.length) return null;
+    if (!users.length) return null;
 
-  const user = users[0];
+    const user = users[0];
 
-  return {
-    id: user.id,
-    name: user.name,
-    password: user.password,
-  };
+    return {
+        id: user.id,
+        name: user.name,
+        password: user.password,
+    };
 };
 
 export const authOptions: AuthConfig = {
-  secret: process.env.FILESHARE_SECRET || "devSecret",
-  session: { strategy: "jwt", maxAge: 365 * 24 * 60 * 60 },
-  theme: { colorScheme: "auto", brandColor: "#FF6961" },
-  callbacks: {
-    jwt: ({ token, account, user }) => {
-      if (account) {
-        token.id = user.id;
-      }
-      return token;
-    },
-    session: ({ session, token }) => {
-      if (session.user) session.user.id = token.id as string;
-      return session;
-    },
-  },
-  providers: [
-    CredentialsProvider({
-      name: "credentials",
-      credentials: {
-        username: { label: "Username", autocomplete: "username" },
-        password: {
-          label: "Password",
-          type: "password",
-          autocomplete: "current-password",
+    secret: process.env.FILESHARE_SECRET || "devSecret",
+    session: { strategy: "jwt", maxAge: 365 * 24 * 60 * 60 },
+    theme: { colorScheme: "auto", brandColor: "#FF6961" },
+    callbacks: {
+        jwt: ({ token, account, user }) => {
+            if (account) {
+                token.id = user.id;
+            }
+            return token;
         },
-      },
-      authorize: (credentials) =>
-        new Promise(async (resolve) => {
-          if (!credentials || !credentials.username || !credentials.password)
-            return null;
+        session: ({ session, token }) => {
+            if (session.user) session.user.id = token.id as string;
+            return session;
+        },
+    },
+    providers: [
+        CredentialsProvider({
+            name: "credentials",
+            credentials: {
+                username: { label: "Username", autocomplete: "username" },
+                password: {
+                    label: "Password",
+                    type: "password",
+                    autocomplete: "current-password",
+                },
+            },
+            authorize: (credentials) =>
+                new Promise(async (resolve) => {
+                    if (
+                        !credentials ||
+                        !credentials.username ||
+                        !credentials.password
+                    )
+                        return null;
 
-          const username = credentials.username as string;
-          const password = credentials.password as string;
+                    const username = credentials.username as string;
+                    const password = credentials.password as string;
 
-          let user = await getUser(username);
+                    let user = await getUser(username);
 
-          if (!user) {
-            const hash = await bcrypt.hash(password, 12);
+                    if (!user) {
+                        const hash = await bcrypt.hash(password, 12);
 
-            if (!hash) return null;
+                        if (!hash) return null;
 
-            user = await db.user.create({
-              data: {
-                name: username,
-                password: hash,
-              },
-              select: {
-                id: true,
-                name: true,
-                password: true,
-              },
-            });
-          } else if (!(await bcrypt.compare(password, user.password))) {
-            return resolve(null);
-          }
+                        user = await db.user.create({
+                            data: {
+                                name: username,
+                                password: hash,
+                            },
+                            select: {
+                                id: true,
+                                name: true,
+                                password: true,
+                            },
+                        });
+                    } else if (
+                        !(await bcrypt.compare(password, user.password))
+                    ) {
+                        return resolve(null);
+                    }
 
-          if (!user) return resolve(null);
-          return resolve({
-            id: user.id,
-            name: user.name,
-          });
+                    if (!user) return resolve(null);
+                    return resolve({
+                        id: user.id,
+                        name: user.name,
+                    });
+                }),
         }),
-    }),
-  ],
+    ],
 };
 
 export default NuxtAuthHandler(authOptions, runtimeConfig);
--- server/api/upload/index.post.ts
+++ server/api/upload/index.post.ts
@@ -1,33 +1,33 @@
-import { randomUUID } from "node:crypto";
+import { randomBytes } from "node:crypto";
 import z from "zod";
 
 import getServerSession from "~/server/getServerSession";
 import db from "~/server/db";
 
 const bodySchema = z.object({
-  name: z.string(),
-  extension: z.string(),
-  private: z.boolean().default(false),
+    name: z.string(),
+    extension: z.string(),
+    private: z.boolean().default(false),
 });
 
 export default defineEventHandler(async (e) => {
-  const body = await readValidatedBody(e, (body) =>
-    bodySchema.parseAsync(body)
-  );
-  const session = await getServerSession(e);
+    const body = await readValidatedBody(e, (body) =>
+        bodySchema.parseAsync(body)
+    );
+    const session = await getServerSession(e);
 
-  if (!session || !session.user || !session.user.id)
-    return setResponseStatus(e, 401);
+    if (!session || !session.user || !session.user.id)
+        return setResponseStatus(e, 401);
 
-  const fullFileName = randomUUID() + body.extension;
+    const fullFileName = randomBytes(7).toString("base64url") + body.extension;
 
-  return db.file.create({
-    data: {
-      id: fullFileName,
-      ownerId: session.user.id,
-      name: body.name,
-      private: body.private,
-    },
-    select: { id: true },
-  });
+    return db.file.create({
+        data: {
+            id: fullFileName,
+            ownerId: session.user.id,
+            name: body.name,
+            private: body.private,
+        },
+        select: { id: true },
+    });
 });
