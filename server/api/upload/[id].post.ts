2e6322fb3f0d02f630e07a067c3ae3bd52acc339
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2024-10-26 19:34:28 +0300

    Formatted code

--- server/api/upload/[id].post.ts
+++ server/api/upload/[id].post.ts
@@ -9,112 +9,118 @@ const filesPath = process.env.FILESHARE_FILES_PATH;
 const thumbnailsPath = process.env.FILESHARE_THUMBNAILS_PATH;
 
 function humanReadableFilesize(bytes: number) {
-  const sizes = ["B", "KB", "MB", "GB", "TB"];
+    const sizes = ["B", "KB", "MB", "GB", "TB"];
 
-  if (bytes === 0) return "0 B";
+    if (bytes === 0) return "0 B";
 
-  const i = parseInt(String(Math.floor(Math.log(bytes) / Math.log(1024))));
+    const i = parseInt(String(Math.floor(Math.log(bytes) / Math.log(1024))));
 
-  return `${(Math.round(100 * (bytes / Math.pow(1024, i))) / 100).toFixed(1)} ${
-    sizes[i]
-  }`;
+    return `${(Math.round(100 * (bytes / Math.pow(1024, i))) / 100).toFixed(1)} ${
+        sizes[i]
+    }`;
 }
 
 export default defineEventHandler(async (e) => {
-  const id = getRouterParam(e, "id");
-  const session = await getServerSession(e);
+    const id = getRouterParam(e, "id");
+    const session = await getServerSession(e);
 
-  if (!id) return setResponseStatus(e, 400);
-  if (!session || !session.user) return setResponseStatus(e, 401);
+    if (!id) return setResponseStatus(e, 400);
+    if (!session || !session.user) return setResponseStatus(e, 401);
 
-  let upload = await db.file.findFirst({
-    where: { id, ownerId: session.user.id },
-  });
+    let upload = await db.file.findFirst({
+        where: { id, ownerId: session.user.id },
+    });
 
-  if (!upload) return setResponseStatus(e, 404);
+    if (!upload) return setResponseStatus(e, 404);
 
-  const multerUpload = multer({
-    storage: multer.diskStorage({
-      destination: (req, file, cb) => {
-        if (!filesPath) throw new Error("No path for files defined");
-        cb(null, filesPath);
-      },
-      filename: async (req, file, cb) => {
-        cb(null, id);
-      },
-    }),
-    fileFilter: (req, file, cb) => {
-      req.on("close", () => {
-        if (!req.complete) {
-          db.file
-            .delete({ where: { id: upload.id } })
-            .catch(console.error)
-            .finally(() => cb(new Error("Upload cancelled")));
-        }
-      });
+    const multerUpload = multer({
+        storage: multer.diskStorage({
+            destination: (_, __, cb) => {
+                if (!filesPath) throw new Error("No path for files defined");
+                cb(null, filesPath);
+            },
+            filename: async (_, __, cb) => {
+                cb(null, id);
+            },
+        }),
+        fileFilter: (req, file, cb) => {
+            req.on("close", () => {
+                if (!req.complete) {
+                    db.file
+                        .delete({ where: { id: upload.id } })
+                        .catch(console.error)
+                        .finally(() => cb(new Error("Upload cancelled")));
+                }
+            });
 
-      const blockedFileTypes = ["text/html", "application/x-httpd-php"];
+            const blockedFileTypes = ["text/html", "application/x-httpd-php"];
 
-      blockedFileTypes.includes(file.mimetype)
-        ? cb(new Error("These file types are not accepted"))
-        : cb(null, true);
-    },
-  });
-
-  // @ts-ignore
-  return callNodeListener(multerUpload.single("file"), e.node.req, e.node.res)
-    .then(async () => {
-      const file = (e.node.req as any).file as {
-        originalname: string;
-        mimetype: string;
-        size: number;
-      };
-
-      let width = 0;
-      let height = 0;
-
-      if (file.mimetype.includes("video")) {
-        try {
-          const dimensions: FfprobeData = await new Promise((resolve) =>
-            ffmpeg(path.join(filesPath!, id)).ffprobe((err, data) =>
-              resolve(data)
-            )
-          );
-
-          width = dimensions.streams[0].width!;
-          height = dimensions.streams[0].height!;
-
-          ffmpeg(path.join(filesPath!, id))
-            .screenshot({
-              filename: `${id}.png`,
-              folder: thumbnailsPath,
-              timestamps: [0],
-              size: "550x309",
-            })
-            .run();
-        } catch (err) {
-          console.error(err);
-        }
-      }
-
-      await db.file.update({
-        where: { id, ownerId: session!.user!.id },
-        data: {
-          type: file.mimetype,
-          size: file.size,
-          width,
-          height,
-          sizeString: humanReadableFilesize(file.size),
+            blockedFileTypes.includes(file.mimetype)
+                ? cb(new Error("These file types are not accepted"))
+                : cb(null, true);
         },
-      });
+    });
 
-      return setResponseStatus(e, 201);
-    })
-    .catch((err) =>
-      createError({
-        message: err.message,
-        statusCode: 422,
-        statusMessage: "Entity cannot be processed",
-      })
-    );
+    // @ts-ignore
+    return callNodeListener(multerUpload.single("file"), e.node.req, e.node.res)
+        .then(async () => {
+            const file = (e.node.req as any).file as {
+                originalname: string;
+                mimetype: string;
+                size: number;
+            };
+
+            let width = 0;
+            let height = 0;
+
+            if (file.mimetype.includes("video")) {
+                try {
+                    const dimensions: FfprobeData = await new Promise(
+                        (resolve, reject) =>
+                            ffmpeg(path.join(filesPath!, id)).ffprobe(
+                                (err, data) => {
+                                    if (err) {
+                                        reject(err);
+                                    }
+                                    resolve(data);
+                                }
+                            )
+                    );
+
+                    width = dimensions.streams[0].width!;
+                    height = dimensions.streams[0].height!;
+
+                    ffmpeg(path.join(filesPath!, id))
+                        .screenshot({
+                            filename: `${id}.png`,
+                            folder: thumbnailsPath,
+                            timestamps: [0],
+                            size: "550x309",
+                        })
+                        .run();
+                } catch (err) {
+                    console.error(err);
+                }
+            }
+
+            await db.file.update({
+                where: { id, ownerId: session!.user!.id },
+                data: {
+                    type: file.mimetype,
+                    size: file.size,
+                    width,
+                    height,
+                    sizeString: humanReadableFilesize(file.size),
+                },
+            });
+
+            return setResponseStatus(e, 201);
+        })
+        .catch((err) =>
+            createError({
+                message: err.message,
+                statusCode: 422,
+                statusMessage: "Entity cannot be processed",
+            })
+        );
 });
