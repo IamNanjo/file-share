2084eb44c77b5852a9e1bb3c30df0acfd590830f
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2024-10-29 01:13:04 +0200

    Fixed video height

--- components/VideoPlayer.vue
+++ components/VideoPlayer.vue
@@ -3,24 +3,43 @@ defineProps<{ url: string }>();
 </script>
 
 <template>
-  <div class="video-player">
-    <video class="video-player__video" controls :src="url" />
-    <div class="video-player__controls"></div>
-  </div>
+    <div class="video-player">
+        <video class="video-player__video" controls :src="url" />
+        <div class="video-player__controls"></div>
+    </div>
 </template>
 
-<style style="scss">
+<style lang="scss">
 .video-player {
-  display: flex;
-  flex-direction: column;
-  max-width: 90%;
-  height: 80svh;
-
-  &__video {
-  }
-
-  > * {
+    position: relative;
+    display: flex;
+    flex-direction: column;
     width: 100%;
-  }
+    max-height: calc(80svh - 5em);
+    overflow: hidden;
+
+    > * {
+        width: 100%;
+    }
+
+    &__video {
+        max-height: calc(80svh - 5em);
+    }
+
+    &__controls {
+        position: absolute;
+        bottom: -2em;
+        left: 0;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        width: 100%;
+        background-color: rgba(0, 0, 0, 0.8);
+        transition: all 0.3s ease;
+    }
+
+    &:hover > &__controls {
+        bottom: 0;
+    }
 }
 </style>
