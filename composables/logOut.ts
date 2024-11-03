452e750952e6b1e0754bcd9fb632648da763a093
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2024-11-04 01:06:02 +0200

    Improved logOut function

--- composables/logOut.ts
+++ composables/logOut.ts
@@ -1,7 +1,14 @@
-export default function signOut() {
+export default async function signOut() {
     const auth = useAuth();
-
-    $fetch("/auth/logout", { method: "post" });
+    const route = useRoute();
 
     auth.value = { authenticated: false };
+    await $fetch("/auth/logout", { method: "post" });
+
+    // Paths that require authentication
+    const authPaths = ["/profile", "/upload"];
+
+    if (authPaths.includes(route.path)) {
+        return navigateTo("/");
+    }
 }
