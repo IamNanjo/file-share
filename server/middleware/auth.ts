0d9ad28cab404034dd2611f250e0b70eff299603
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2024-11-04 01:04:44 +0200

    Simplified code

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,14 +1,12 @@
-import { getSession, type SessionData } from "~/server/session";
-import type { useSession } from "h3";
+import { getSession } from "~/server/session";
 
 declare module "h3" {
     interface H3EventContext {
-        session: Awaited<ReturnType<typeof useSession<SessionData>>> | null;
+        session: Awaited<ReturnType<typeof getSession>> | null;
     }
 }
 
 export default defineEventHandler(async (e) => {
-    if (e.path === "/login") return;
     const session = await getSession(e);
     e.context.session = Object.keys(session.data).length ? session : null;
 });
