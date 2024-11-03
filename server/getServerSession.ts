3d460223b52cc6ee6903c11ec1feef5adf21b702
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2024-11-03 18:46:11 +0200

    Deleted old auth files

--- server/getServerSession.ts
+++
@@ -1,7 +0,0 @@
-import { authOptions } from "./api/auth/[...]";
-import { getServerSession } from "#auth";
-
-import type { H3Event, EventHandlerRequest } from "h3";
-
-export default (e: H3Event<EventHandlerRequest>) =>
-  getServerSession(e, authOptions);
