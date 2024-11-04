f4460f567e47f3c327e815ff2296bd23f5ab11c9
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2024-11-04 14:38:23 +0200

    Parse request using Promise.all

--- server/api/comments/[id].put.ts
+++ server/api/comments/[id].put.ts
@@ -8,24 +8,18 @@ export default defineEventHandler(async (e) => {
         return setResponseStatus(e, 401);
     }
 
-    const { id } = await getValidatedRouterParams(
-        e,
-        z.object({
-            id: z.coerce.number().min(1),
-        }).parse
-    );
-    const { content } = await readValidatedBody(
-        e,
-        z.object({
-            content: z.string().min(1),
-        }).parse
-    );
+    const [{ id }, { content }] = await Promise.all([
+        getValidatedRouterParams(
+            e,
+            z.object({ id: z.coerce.number().min(1) }).parse
+        ),
+        readValidatedBody(e, z.object({ content: z.string().min(1) }).parse),
+    ]);
 
-    return db.comment
-        .update({
-            where: { id, authorId: session.data.id },
-            data: { content },
-        })
-        .then(() => setResponseStatus(e, 200))
-        .catch(() => setResponseStatus(e, 500));
+    await db.comment.update({
+        where: { id, authorId: session.data.id },
+        data: { content },
+    });
+
+    return setResponseStatus(e, 200);
 });
