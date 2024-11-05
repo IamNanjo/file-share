6f7bbc38d9a181d21a6101a322a85168b8bc35ca
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2024-11-05 08:04:02 +0200

    Fixed "0 months ago" bug

--- composables/getRelativeTimestamp.ts
+++ composables/getRelativeTimestamp.ts
@@ -13,14 +13,14 @@ export default function getRelativeTimestamp(date: Date) {
     if (diffHrs < 24) return `${diffHrs} hour${diffHrs !== 1 ? "s" : ""} ago`;
 
     const diffDays = Math.floor(diffHrs / 24);
-    if (diffDays < 7) return `${diffDays} day${diffDays !== 1 ? "s" : ""} ago`;
+    if (diffDays <= 7) return `${diffDays} day${diffDays !== 1 ? "s" : ""} ago`;
 
     const diffWeeks = Math.floor(diffDays / 7);
-    if (diffWeeks < 4)
+    if (diffWeeks <= 4)
         return `${diffWeeks} week${diffWeeks !== 1 ? "s" : ""} ago`;
 
     const diffMonths = Math.floor(diffDays / 30.44);
-    if (diffMonths < 12)
+    if (diffMonths <= 12)
         return `${diffMonths} month${diffMonths !== 1 ? "s" : ""} ago`;
 
     const diffYears = Math.floor(diffDays / 365);
