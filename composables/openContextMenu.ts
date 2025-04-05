c6a6933b3886fca644c83c6961ae06c1a25b5da4
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2025-04-05 13:48:53 +0300

    fix: Use pointerup event for more reliability

--- composables/openContextMenu.ts
+++ composables/openContextMenu.ts
@@ -2,9 +2,7 @@ export default function openContextMenu(id: string | number) {
     const contextMenuOpen = useContextMenu();
     contextMenuOpen.value = id;
 
-    document.addEventListener(
-        "pointerdown",
-        () => (contextMenuOpen.value = ""),
-        { once: true }
-    );
+    document.addEventListener("pointerup", () => (contextMenuOpen.value = ""), {
+        once: true,
+    });
 }
